04e1f22896a12cc4c346a0fe47909d0264923a64
Mariana21hch <mariana21hch@gmail.com>
2022-07-25 21:33:29 -0500

    complete code

--- Develop/script.js
+++ Develop/script.js
@@ -1,45 +1,54 @@
-//get current date and break it down to dd mmm yyyy
-var fullToday = new Date();
-var todayMonth = fullToday.getMonth();
-var allMonths = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-var fullStandardDate = fullToday.getDate() + " " + allMonths[todayMonth] + " " + fullToday.getFullYear();
-//get current time
-var currentTime = fullToday.toTimeString().split(' ')[0];
-var para = document.querySelectorAll("p");
-var para_array = Array.from(para);
+
+var fullToday = new Date(); //Todays date object
+var todayMonth = fullToday.getMonth(); //Current Month
+var allMonths = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]; //All Months array in 3 letter format
+var fullStandardDate = fullToday.getDate() + " " + allMonths[todayMonth] + " " + fullToday.getFullYear(); //Standard Date Format for Page title DD MMM YYYY
+var currentTime = fullToday.toTimeString().split(' ')[0]; //get current time
+
 
 $(document).ready(function() {
     //call the change section color function
-    changeTimeColor();
     getTimes();
+
     // display current date
     $('#currentDay').html(fullStandardDate);
 
+    //When clicking on a save button, Save to localStorage
+    $(".saveBtn").click(function() {
+        var inputValue = $(this).closest("section").find("input").val();
+        localStorage.setItem($(this).closest("section").attr("data-time"), inputValue);
+    });
 
+    //on Page load, load values from Storage into the input fields
+    $("section").each(function() {
+        var keyName = $(this).attr("data-time");
+        var keyValue = localStorage.getItem(keyName);
+        $(this).find("input").val(keyValue);
+    });
 });
 
-//create a function that 
-function changeTimeColor(){
-    para.forEach(function(elem){
-        //console.log(elem.dataset.time);
-        if (elem.dataset.time < currentTime){
-            //$(".colorChange").addclass("past");
-        }
-    });
-};
+//create a function to change the time slots from past/ present and future 
+// get the data-time attribute 
+// need to find a way to compare real time and the data attribute to change the color and add the class
+// create two dates one with real time and one with data-time 
+// take away the min by .split to only compare hours
+//create if statements for each to add the class for each
+// call the function at the doc.ready for it to run once the page starts. 
 
 function getTimes() {
-    $("section").each(function() {
-        var sectionAttr = $(this).attr("data-time");
-        var blockTime = new Date(fullToday.getFullYear(),(fullToday.getMonth()),fullToday.getDate(), sectionAttr);
-        var presentTime = new Date(fullToday.getFullYear(), fullToday.getMonth(), fullToday.getDate(), currentTime.split(":")[0]);
+    var presentTime = new Date(fullToday.getFullYear(), fullToday.getMonth(), fullToday.getDate(), currentTime.split(":")[0]); //Create Date object with top of the hour and 0 minutes
+    $("section").each(function() { //For each section element
+        var sectionAttr = $(this).attr("data-time"); //Grab attribute value of 'data-time' in that section element
+        var blockTime = new Date(fullToday.getFullYear(),(fullToday.getMonth()),fullToday.getDate(), sectionAttr); //Create a date object for the current block time
 
-        if (blockTime < presentTime) {
-            $(this).find("div:first").addClass("past");
-        } else if (blockTime > presentTime) {
-            $(this).find("div:first").addClass("future");
-        } else {
-            $(this).find("div:first").addClass("present");
+        if (blockTime < presentTime) { //If the block time is before the current top of the hour
+            $(this).find("div:first").addClass("past"); //Add the 'past' Class
+        } else if (blockTime > presentTime) { //If the block time is after the current top of the hour
+            $(this).find("div:first").addClass("future"); //Add the 'future' Class
+        } else { //else
+            $(this).find("div:first").addClass("present"); //Add the 'present' Class
         }
     });
 }
+
+
--- README.md
+++ README.md
@@ -36,10 +36,27 @@ List on what I need to do from start to finish
 1. in HTML create either a table or sections for each time  {complete}
     1.a. I created a sectition and added/ edited the CSS
 2. in HTML center section make it a text box {complete}
-    2.a. I added the alresdy created css to the text box
+    2.a. I added the alresdy created css to the text box {completed}
 3. in script i need to get date and add a <p> tag to display it on the site
     3.a. there is a slot created for todays date (this part can be obmited)
-4. in script get time and create a function if time less than or equal to current time then grey out section
+    3.b. create a script to display current date {complete}
+4. in script get time and create a function if time less than or equal to current time then grey out section/ the present time is in red and the future is in green {complete}
+
 5. in script .click on the save text to the time 
 6. on script save to local storage
 7. should be able to still view after share  (test)
+
+
+
+resorses:
+https://www.linuxscrew.com/javascript-compare-dates#:~:text=Compare%20JavaScript%20Dates%20%28Day%2FMinute%2FHour%2FBefore%2FAfter%29%2C%20With%20Examples%201%20Using,Day%2C%20Month%2C%20Year.%20...%203%20Moment%20JS.%20
+
+https://www.w3schools.com/TAGS/att_global_data.asp
+
+https://www.skillsugar.com/how-to-use-jquery-attr-to-get-set-data-attribute-values#:~:text=To%20set%20and%20get%20data%20on%20an%20HTML,attribute%20like%20this%3A%20%3Cbutton%20id%3D%22button%22%20type%3D%22button%22%20data-color%3D%22blue%22%3EBlue%20button%3C%2Fbutton%3E
+
+https://www.geeksforgeeks.org/how-to-get-seconds-since-epoch-in-javascript/#:~:text=The%20getTime()%20method%20in,number%20of%20seconds%20since%20epoch.
+
+https://www.w3schools.com/jsref/jsref_settime.asp
+
+https://stackoverflow.com/questions/37571763/javascript-split-function-not-working#:~:text=The%20reason%20yours%20isn%27t%20working%20is%20because%20when,highlight%20why%20you%20are%20getting%20that%20error.%20Share
